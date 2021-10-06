9cd0baf414b28e1aba32b5543b41dc5053050e4b
Alexabelyatskaya <59415457+Alexabelyatskaya@users.noreply.github.com>
2021-10-06 11:13:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         IndexService service = new IndexService();
         double height = 1.67;
-        int weight = 59;
+        double weight = 59;
         double index = service.calculate(height, weight);
         System.out.println(index);
     }
